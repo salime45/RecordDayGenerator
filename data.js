c3ab9615c160f07de79d1bcbfb3e7a99783b89fc
imonje <Oscuridad55>
2018-06-21 11:26:39 +0200

    add smart data

---
+++ data.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+    getDays: function (year, month) {
+        var arr = [];
+
+        for (var i = 1; i <= daysInMonth(month, year); i++) {
+
+            var date = new Date(year + '-' + (month).pad(2) + '-' + (i).pad(2))
+
+            // console.log(daysInMonth(month, year))
+
+            var day = {
+                "dia": i,
+            };
+
+            //comprobamos que no sea sabado o domingo
+            if (date.getDay() > 0 && date.getDay() < 6) {
+                day.entrada = "09:00"
+                day.salida = "14:00"
+                day.horas = 5
+            }
+
+            arr.push(day);
+        }
+        return arr
+    },
+
+}
+
+/**
+ * Get the numer of days the month and year
+ * Example  daysInMonth(2, 2016) = 29
+ * @param {*} month 
+ * @param {*} year 
+ */
+function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
+}
+
+/**
+ * Leading zeros a la izq.
+ * Example (123).pad(6) = 000123
+ * @param {*} size 
+ */
+Number.prototype.pad = function (size) {
+    var s = String(this);
+    while (s.length < (size || 2)) { s = "0" + s; }
+    return s;
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const handlebars = require('handlebars');
 const fs = require('fs');
 const pdf = require('html-pdf');
+const data = require('./data');
 
 // const inFile = './report.hbs';
 const inFile = './report.html';
@@ -23,37 +24,23 @@ const outPdf = './report.pdf';
 // console.log(result);
 // fs.writeFileSync(outFile, result);
 
-
+// console.log(data.getDays())
 // data to apply to template file
 const user = {
   "date": new Date().toISOString(),
   "title": "Guillaume",
   "body": "bodyyyyyyyyy",
-  "purchases": [
-    {
-      "product": "pro",
-      "quantity": "5",
-      "formatDateTime ": "5",
-      "x ": "5"
-    }, {
-      "product": "prosdf",
-      "quantity": "534",
-      "formatDateTime ": "5",
-      "x ": "5"
-    }
-  ]
+  "purchases": data.getDays(2018, 6)
 };
 const options = {
   "format": 'A4',
-  "orientation": "landscape",
+  "orientation": "portrait",
 
 };
-// const localTemplate = path.join(os.tmpdir(), 'localTemplate.html');
-// const localPDFFile = path.join(os.tmpdir(), 'localPDFFile.pdf');
 
 const source = fs.readFileSync(inFile, 'utf8');
 const html = handlebars.compile(source)(user);
-console.log("template compiled with user data", html);
+// console.log("template compiled with user data", html);
 
 pdf.create(html, options).toFile(outPdf, function (err, res) {
   if (err) {
@@ -65,8 +52,3 @@ pdf.create(html, options).toFile(outPdf, function (err, res) {
 });
 
 
-// const myPdfFile = admin.storage().bucket().file('/test/Arbeitsvertrag.pdf');
-// const doc = new pdfkit();
-// const stream = doc.pipe(myPdfFile.createWriteStream());
-// doc.fontSize(25).text('Test 4 PDF!', 100, 100);
-// doc.end();
